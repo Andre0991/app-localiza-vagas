27341b75bc68333e59a8004e6667216306921ffc
Andre0991 <andre.peric.tavares@gmail.com>
2016-11-04 15:42:27 -0200

    Add activities

--- android-studio-projeto/app/src/main/java/com/example/andreperictavares/projetocompartilhamentovagasdispmoveis/MainActivity.java
+++ android-studio-projeto/app/src/main/java/com/example/andreperictavares/projetocompartilhamentovagasdispmoveis/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.andreperictavares.projetocompartilhamentovagasdispmoveis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static boolean IS_REGISTERED = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (!IS_REGISTERED){
+            goToRegisterScreen();
+        }
     }
+
+    public void goToRegisterScreen() {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
 }
