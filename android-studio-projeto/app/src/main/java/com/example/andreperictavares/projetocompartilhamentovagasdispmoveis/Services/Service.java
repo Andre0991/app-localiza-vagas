99893c0068aca5223b1f62d18535b64eebd1d95e
Andre0991 <andre.peric.tavares@gmail.com>
2016-11-18 19:55:38 -0200

    Adiciona classe para constantes

---
+++ android-studio-projeto/app/src/main/java/com/example/andreperictavares/projetocompartilhamentovagasdispmoveis/Services/Service.java
@@ -0,0 +1,12 @@
+package com.example.andreperictavares.projetocompartilhamentovagasdispmoveis.Services;
+
+/**
+ * Created by andreperictavares on 18/11/2016.
+ */
+
+public class Service {
+
+    public static final String API_VERSION = "v1";
+    public static final String API_ADDRESS = "http://192.168.0.101:3000/api/";
+
+}
