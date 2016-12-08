93d2159c991fa437ec18fb4da9a36374da629088
Andre0991 <andre.peric.tavares@gmail.com>
2016-12-08 20:08:49 -0200

    Adiciona calçada com ID no serviço.

--- android-studio-projeto/app/src/main/java/com/example/andreperictavares/projetocompartilhamentovagasdispmoveis/Network/CalcadaServices.java
+++ android-studio-projeto/app/src/main/java/com/example/andreperictavares/projetocompartilhamentovagasdispmoveis/Network/CalcadaServices.java
@@ -71,8 +71,6 @@ public class CalcadaServices {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
-
     }
 
 }
--- node/config/secrets.js
+++ node/config/secrets.js
@@ -1,5 +1,5 @@
 module.exports = {
     jwt_secret: "$2a$10$cYyogjl49.IcDtwu/dy0a.fB3FHF2OP4uQYGxCGguiQa.vP5nv83.",
-    google_distance_key: ""
+    google_distance_key: "AIzaSyBqcGjUtqH92bxlMe9g3d2p9ibYC8OAdv8"
 }
 
