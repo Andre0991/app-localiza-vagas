84a78921822fc3e96a03bed5473993357abf4890
Andre0991 <andre.peric.tavares@gmail.com>
2016-12-11 19:25:56 -0200

    Adiciona listeners ao mapa

--- android-studio-projeto/app/src/main/java/com/example/andreperictavares/projetocompartilhamentovagasdispmoveis/Activities/SearchFreeParkingLocationActivity.java
+++ android-studio-projeto/app/src/main/java/com/example/andreperictavares/projetocompartilhamentovagasdispmoveis/Activities/SearchFreeParkingLocationActivity.java
@@ -5,10 +5,20 @@ import android.location.Location;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 import com.example.andreperictavares.projetocompartilhamentovagasdispmoveis.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class SearchFreeParkingLocationActivity extends LocationActivity {
+
+    public static final String TAG = "SearchFreePkgLocation";
 
     double latitude;
     double longitude;
@@ -19,12 +29,27 @@ public class SearchFreeParkingLocationActivity extends LocationActivity {
         setContentView(R.layout.activity_search_free_parking_location);
         super.mRequestingLocationUpdates = true;
 
-        MapFragment mMapFragment = MapFragment.newInstance();
-        FragmentTransaction fragmentTransaction =
-                getFragmentManager().beginTransaction();
-        fragmentTransaction.add(R.id.activity_search_free_parking_location, mMapFragment);
-        fragmentTransaction.commit();
+        ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                // add sydney
+                LatLng sydney = new LatLng(-34, 151);
+                final Marker sydneyMarker = googleMap.addMarker((new MarkerOptions()
+                .position(sydney)
+                .title("Marker")));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
+                googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                    @Override
+                    public boolean onMarkerClick(Marker marker) {
+                        if (marker.equals(sydneyMarker)) {
+                            Log.d(TAG, "clicou!");
+                        }
+                        return true;
+                    }
+                });
+            }
+        });
     }
 
     @Override
